ab87932edb6bf9a0d5bc79ea26f44c6f8cf214f5
cyfi-s <c.smeltzer@ufl.edu>
2024-04-16 16:32:51 -0700

    Added preliminary graph functions.

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,4 +1,19 @@
 //
 // Created by gator on 4/15/2024.
 //
+#include "graph.h"
 
+void graph::connectSimilarGames(const game &startGame, const vector<game>& similarGames) {
+    for(const game& similarGame : similarGames)
+    {
+        addEdge(startGame, similarGame);
+        addEdge(similarGame, startGame);
+        /*
+        if (List[similarGame].empty())
+        {
+            connectSimilarGames(similarGame,similarGame.similarGamesIDs);
+        }
+         /*
+    }
+
+}
--- src/graph.h
+++ src/graph.h
@@ -4,22 +4,37 @@
 
 #ifndef PROJECT3_GRAPH_H
 #define PROJECT3_GRAPH_H
+
 #include <string>
 #include <unordered_map>
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <game.h>
+#include "game.h"
 using namespace std;
 
 class graph {
 private:
-    unordered_map<game, vector<game>> List;
+    unordered_map<game, vector<game>> List; // Stores adjacency list of games
+
 public:
-    graph() = default;
+    graph() = default; // Default constructor
+
+    // Adds an edge between two games, creating a bidirectional link
     void addEdge(const game, const game);
 
+    // Recursively connects all games that are similar to each other
+    void connectSimilarGames(const game& startGame, const vector<game>& similarGames);
+
+    // Connects all games within the same genre
+    void connectSimilarGenres(const string genre);
+
+    // Connects all games released in the same year
+    void connectSameReleaseDate(int year);
+
+    // Connects all games developed by the same company
+    void connectCompanyGames(string company);
 };
 
-
 #endif //PROJECT3_GRAPH_H
+
