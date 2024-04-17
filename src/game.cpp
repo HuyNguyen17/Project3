fff37bdece23271748f2711f5e006d292d9c4e07
HuyNguyen17 <genericnguyenhuy17@gmail.com>
2024-04-16 22:48:10 -0400

    function definitions for getters

--- src/game.cpp
+++ src/game.cpp
@@ -4,6 +4,31 @@ bool game::operator==(const game &other) const {
     return other.id == this->id;
 }
 
+// Getters
+vector<Company> game::getCompanies() const {
+    return companies;
+}
+
+set<Genre> game::getGenres() const {
+    return genres;
+}
+
+unsigned int game::getID() const {
+    return id;
+}
+
+string game::getName() const {
+    return name;
+}
+
+unsigned int game::getReleaseDate() const {
+    return release_date;
+}
+
+vector<unsigned int> game::getSimilarGames() const {
+    return similarGamesIDs;
+}
+
 size_t gameHash::operator()(const game &game) const {
     return game.getID();
 }
