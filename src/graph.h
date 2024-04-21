570b024b000a3d92c7521e21ccc1cd4f5a17655e
cyfi-s <c.smeltzer@ufl.edu>
2024-04-21 10:32:24 -0700

    Added better comments to functions

--- src/graph.h
+++ src/graph.h
@@ -53,20 +53,34 @@ public:
 
     int getNumEdges() const;
     int getNumNodes() const;
+
+    // Returns the ID of a game when searching by the game's _name.
     int getIDfromSearching(string &_name);
+
+
+    //_name is the main searchBar, maxDepth specifies the maximum depth to which the user wants to explore connected games
     void BFSprintConnectedGames(string _name, int maxDepth);
     void DFSprintConnectedGames(string _name, int maxDepth);
-    bool gamesConnected(string &_name1, string &_name2, int searchPath);
-    void searchByGenre(string _genre);
-    void printAllGenre();
-    void searchByCompany(string _company);
-    void printAllCompany();
-    void searchByReleaseDate(string _releaseDate);
 
     //Returns a Vector of BFS
     vector<shared_ptr<game>> getConnectedGamesBFS(string _name, int maxDepth);
     //Returns a Vector of DFS
     vector<shared_ptr<game>> getConnectedGamesDFS(string _name, int maxDepth);
+
+    //_name1 is the main searchBar (source), _name2 is the destination, searchPath, 0 is BFS, 1 is DFS.
+    bool gamesConnected(string &_name1, string &_name2, int searchPath);
+    //Prints all the games in a specific Genre
+    void searchByGenre(string _genre);
+    //Prints all the available Genres to search from.
+    void printAllGenre();
+    //Prints all the games in a specific company
+    void searchByCompany(string _company);
+    //Prints all the available company to search from.
+    void printAllCompany();
+    //Prints all the games in a specific release date
+    void searchByReleaseDate(string _releaseDate);
+
+
 };
 
 #endif //PROJECT3_GRAPH_H
